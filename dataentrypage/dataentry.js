b3f188e12608606b3fce03b001cc31bcfae68939
mangoud2005 <bodymangoud@gmail.com>
2025-09-28 17:05:25 +0300

    ..

--- dataentrypage/dataentry.js
+++ dataentrypage/dataentry.js
@@ -1,26 +1,26 @@
-window.onload = function () {
-    // امنع التمرير قبل تسجيل الدخول
-    document.body.style.overflow = "hidden";
+// window.onload = function () {
+//     // امنع التمرير قبل تسجيل الدخول
+//     document.body.style.overflow = "hidden";
   
-    // عرض المودال أول ما الصفحة تفتح
-    document.getElementById("loginModal").style.display = "block";
-  };
+//     // عرض المودال أول ما الصفحة تفتح
+//     document.getElementById("loginModal").style.display = "block";
+//   };
   
-  function login() {
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
+//   function login() {
+//     const username = document.getElementById("username").value.trim();
+//     const password = document.getElementById("password").value.trim();
   
-    const validUser = "dataentry1";
-    const validPass = "1234";
+//     const validUser = "dataentry1";
+//     const validPass = "1234";
   
-    if (username === validUser && password === validPass) {
-      document.getElementById("loginModal").style.display = "none";
-      document.getElementById("login-error").style.display = "none";
-      document.body.style.overflow = "auto"; // السماح بالتمرير بعد الدخول
-    } else {
-      document.getElementById("login-error").style.display = "block";
-    }
-  }
+//     if (username === validUser && password === validPass) {
+//       document.getElementById("loginModal").style.display = "none";
+//       document.getElementById("login-error").style.display = "none";
+//       document.body.style.overflow = "auto"; // السماح بالتمرير بعد الدخول
+//     } else {
+//       document.getElementById("login-error").style.display = "block";
+//     }
+//   }
 
   /////
   
